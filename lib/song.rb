871969416b56f704c9bd9bcf845239cdc551f3b0
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-05 21:18:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,15 +8,12 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def initialize
-    self.class.all << self
-  end
 
   def self.all
     @@songs
