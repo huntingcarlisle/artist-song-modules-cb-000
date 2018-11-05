6f82455d17121aea164fbd7c6c5ae24e5a755c40
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-05 21:07:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,7 @@ require_relative './concerns/memorable'
 class Song
   
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   
   attr_accessor :name
   attr_reader :artist
@@ -12,10 +13,6 @@ class Song
 
   def initialize
     @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
   end
 
   def self.all
