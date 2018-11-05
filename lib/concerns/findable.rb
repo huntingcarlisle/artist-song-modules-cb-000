41b25590156d68ae3ccd8c8ee4ef5935ab23daf1
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-05 21:04:32 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,11 @@
 module Findable
   module ClassMethods
-   def find_by_name
+    def find_by_name
      
-   end
-    
+    end
+    def self.find_by_name(name)
+      @@artists.detect{|a| a.name == name}
+    end  
+  
   end
 end
